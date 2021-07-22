725c05cbf3abbeaace73bf3bdc5832e59596c9e6
User <user@Users-MacBook-Pro.local>
2021-07-22 18:59:29 +0300

    fix exercise4 changed permitpasswordlogin

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -4,7 +4,7 @@
 ### we need to add each server ip and dns name to each hosts file on the crosponding server and permit password authentication in ssh config file###
  
 sudo sed -i '$ a 192.168.100.11 server2' /etc/hosts
-sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#PasswordAuthentication no /PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sleep 5
 
 sudo systemctl restart sshd
